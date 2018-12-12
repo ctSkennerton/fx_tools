487c2a824db3845640a18f610322d6f4465ff709
Connor Skennerton <connor@secondgenome.com>
2018-12-11 16:54:10 -0800

    remove identical sequences as well as names

--- fxuniq.c
+++ fxuniq.c
@@ -37,20 +37,24 @@ void stk_printseq(const kseq_t *s, int line_len)
 	}
 }
 
-void process(const char * file, khash_t(str) *h) 
+void process(const char * file, khash_t(str) *h, int uniq_seq) 
 {
-	gzFile fp;
-	kseq_t *seq;
+    gzFile fp;
+    kseq_t *seq;
     int absent, l;
     khint_t k;
-	fp = strcmp(file, "-")? gzopen(file, "r") : gzdopen(fileno(stdin), "r");
-	seq = kseq_init(fp);
-	while ((l = kseq_read(seq)) >= 0) {
-        k = kh_put(str, h, seq->name.s, &absent);
+    fp = strcmp(file, "-")? gzopen(file, "r") : gzdopen(fileno(stdin), "r");
+    seq = kseq_init(fp);
+    while ((l = kseq_read(seq)) >= 0) {
+        char * comparison = seq->name.s;
+        if (uniq_seq) {
+            comparison = seq->seq.s;
+        }
+        k = kh_put(str, h, comparison, &absent);
         if (absent) 
         {
             stk_printseq(seq, 0);
-            kh_key(h, k) = strdup(seq->name.s);
+            kh_key(h, k) = strdup(comparison);
         } 
     }
     for (k = 0; k < kh_end(h); ++k)
@@ -60,8 +64,8 @@ void process(const char * file, khash_t(str) *h)
             free((char*)kh_key(h, k));
         }
     }
-	kseq_destroy(seq);
-	gzclose(fp);
+    kseq_destroy(seq);
+    gzclose(fp);
 }
 
 void help()
@@ -70,6 +74,7 @@ void help()
     fprintf(stderr, "Usage:   fxuniq [options] <in.fx>...\n");
     fprintf(stderr, "Options:\n\n");
     fprintf(stderr, "-%-10c%s\n", 'f', "ensure uniqueness within each file (allow duplicates beteen files)");
+    fprintf(stderr, "-%-10c%s\n", 's', "Compare uniqueness on the sequences, rather than the headers");
     fprintf(stderr, "-%-10c%s\n", 'h', "print this help message");
     fprintf(stderr, "\n\n");
 }
@@ -79,25 +84,27 @@ int main(int argc, char * argv[])
 
     int c;
     int per_file = 0;
-    while ((c = getopt (argc, argv, "fh")) != -1)
+    int uniq_seq = 0;
+    while ((c = getopt (argc, argv, "sfh")) != -1)
     {
         switch(c)
         {
             case 'f': per_file = 1; break;
+            case 's': uniq_seq = 1; break;
             case 'h': help(); return 1; break;
         }
     }
     khash_t(str) *h = kh_init(str);
     if(optind >= argc)
     {
-        process("-", h);
+        process("-", h, uniq_seq);
     }
     else 
     {
         int i;
         for(i = optind; i < argc; ++i)
         {
-            process(argv[i], h);
+            process(argv[i], h, uniq_seq);
             if(per_file) 
             {
                 kh_clear(str, h);
